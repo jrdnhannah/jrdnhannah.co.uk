97128fdadaae923208dbe9b8d980c94c130ea484
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-14 22:48:10 +0100

    [Index] App creation moved to bootstrap

--- web/index.php
+++ web/index.php
@@ -2,11 +2,6 @@
 
 namespace jrdn;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$app = new \Silex\Application;
-
 require_once __DIR__ . '/../app/bootstrap.php';
 
-$app['debug'] = true;
 $app->run();
