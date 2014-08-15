1e4f141c0e82c6b59e2a2c830e9d463286445d40
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-15 22:29:16 +0100

    [Doctrine] Correct path

--- app/config/doctrine.php
+++ app/config/doctrine.php
@@ -3,5 +3,5 @@
 /**
  * Configure Doctrine ORM
  */
-$config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([__DIR__ . '/../../Application/Entity'], $app['debug'], $app['orm.proxies_dir'], null, false);
+$config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([__DIR__ . '/../../src/Application/Entity'], $app['debug'], $app['orm.proxies_dir'], null, false);
 $em = \Doctrine\ORM\EntityManager::create($app['db.options'], $config);
