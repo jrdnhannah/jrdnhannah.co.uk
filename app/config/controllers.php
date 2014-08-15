53c07418130106e4be7a4bd097a8a96f2cff71ef
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-15 13:26:27 +0100

    [Controllers] Register news controller. Remove App::share as not in dev-master of silex

--- app/config/controllers.php
+++ app/config/controllers.php
@@ -1,19 +1,24 @@
 <?php
 
-$app['site.controller'] = $app->share(function () use ($app) {
-        return new \Application\Controller\SiteController($app['twig']);
-    }
-);
+$app['site.controller'] = function () use ($app) {
+    return new \Application\Controller\SiteController($app['twig']);
+};
 
-$app['contact.controller'] = $app->share(function () use ($app) {
-        return new \Application\Controller\ContactController(
-            $app['twig'],
-            $app['form.factory'],
-            $app['mailer'],
-            $app['mail.to'],
-            $app['mail.from']
-        );
-    }
-);
+$app['contact.controller'] = function () use ($app) {
+    return new \Application\Controller\ContactController(
+        $app['twig'],
+        $app['form.factory'],
+        $app['mailer'],
+        $app['mail.to'],
+        $app['mail.from']
+    );
+};
+
+$app['news.controller'] = function() use ($app) {
+    return new \Application\Controller\NewsController(
+        $app['twig'],
+        $app['orm.em']
+    );
+};
 
 return $app;
