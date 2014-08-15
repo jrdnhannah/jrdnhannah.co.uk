ef472b66bec212cd2e5a7447a555f79734f9b6b7
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-15 13:26:00 +0100

    [Routes] Create param converter, add news routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,7 +1,26 @@
 <?php
 
-$app->get('/',         'site.controller:indexAction')->bind('route.home');
-$app->get('/contact',  'contact.controller:contactAction')->bind('route.contact');
-$app->post('/contact', 'contact.controller:handleContactAction');
+$entityParamConverter = function($entityClass, $value, $field = 'id') use ($app) {
+    return $app['orm.em']->getRepository($entityClass)
+                         ->findOneBy([$field => $value]);
+};
+
+$app->get('/',                  'site.controller:indexAction')
+    ->bind('route.home');
+
+$app->get('/contact',           'contact.controller:contactAction')
+    ->bind('route.contact');
+
+$app->post('/contact',          'contact.controller:handleContactAction');
+
+$app->get('/news',              'news.controller:showArticleListAction')
+    ->bind('route.news');
+
+$app->get('/news/{article}',    'news.controller:showArticleAction')
+    ->bind('route.news_article')
+    ->convert('article', function($article) use ($entityParamConverter) {
+            return $entityParamConverter('Application\Entity\Article', $article);
+        }
+    );
 
 return $app;
