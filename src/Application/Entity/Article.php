ac8aafd1480324b7d46aea84fe8106515e74d878
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-15 22:31:22 +0100

    [Article] Add timestamps

--- src/Application/Entity/Article.php
+++ src/Application/Entity/Article.php
@@ -34,6 +34,26 @@ class Article
     private $content;
 
     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_updated", type="datetime")
+     */
+    private $lastUpdated;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime('now');
+        $this->lastUpdated = new \DateTime('now');
+    }
+
+    /**
      * @return int
      */
     public function getId()
@@ -74,6 +94,32 @@ class Article
     public function setTitle($title)
     {
         $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastUpdated()
+    {
+        return $this->lastUpdated;
+    }
+
+    /**
+     * @ORM\PreFlush
+     * @return $this
+     */
+    public function markUpdated()
+    {
+        $this->lastUpdated = new \DateTime('now');
         return $this;
     }
 }
