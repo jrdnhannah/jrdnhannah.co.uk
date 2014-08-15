bc0197558a6bbaa8444a0936fda4a3e8e802bb10
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-15 13:24:50 +0100

    [Providers] Doctrine ORM

--- app/config/providers.php
+++ app/config/providers.php
@@ -2,12 +2,14 @@
 
 $providers = [
     [new \Silex\Provider\TwigServiceProvider, ['twig.path' => __DIR__ . '/../../views']],
+    [new \Silex\Provider\LocaleServiceProvider],
     [new \Silex\Provider\TranslationServiceProvider, ['locale_fallback' => 'en']],
     [new \Silex\Provider\ServiceControllerServiceProvider],
-    [new \Silex\Provider\UrlGeneratorServiceProvider],
+    [new \Silex\Provider\RoutingServiceProvider],
     [new \Silex\Provider\FormServiceProvider],
     [new \Silex\Provider\SwiftmailerServiceProvider],
-    [new \Silex\Provider\DoctrineServiceProvider]
+    [new \Silex\Provider\DoctrineServiceProvider],
+    [new \Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider]
 ];
 
 return $providers;
