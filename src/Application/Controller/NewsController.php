41b56f5e33ff7cf46a6c346c270f710776e961d0
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-17 14:24:59 +0100

    [NewsController] Add CRUD functionality

--- src/Application/Controller/NewsController.php
+++ src/Application/Controller/NewsController.php
@@ -3,7 +3,15 @@
 namespace Application\Controller;
 
 use Application\Entity\Article;
+use Application\Entity\User;
+use Application\Exception\InvalidFormException;
+use Application\User\UserManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class NewsController
 {
@@ -13,14 +21,19 @@ class NewsController
     /** @var \Doctrine\ORM\EntityManagerInterface */
     private $em;
 
+    /** @var \Symfony\Component\Form\FormFactoryInterface */
+    private $form;
+
     /**
      * @param \Twig_Environment $twig
      * @param EntityManagerInterface $em
+     * @param FormFactoryInterface $form
      */
-    public function __construct(\Twig_Environment $twig, EntityManagerInterface $em)
+    public function __construct(\Twig_Environment $twig, EntityManagerInterface $em, FormFactoryInterface $form)
     {
         $this->twig = $twig;
         $this->em   = $em;
+        $this->form = $form;
     }
 
     /**
@@ -29,7 +42,7 @@ class NewsController
     public function showArticleListAction()
     {
         /** @var Article[] $articleList */
-        $articleList = $this->em->getRepository('Application\Entity\Article')->findAll();
+        $articleList = $this->em->getRepository('Application\Entity\Article')->findBy([], ['createdAt' => 'desc']);
 
         return $this->twig->render('news/article_list.html.twig', ['article_list' => $articleList]);
     }
@@ -41,5 +54,95 @@ class NewsController
     public function showArticleAction(Article $article)
     {
         return $this->twig->render('news/article.html.twig', ['article' => $article]);
+    }
+
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function createArticleAction(Request $request)
+    {
+        $handler = function(FormInterface $form) {
+            $this->em->persist($form->getData());
+            $this->em->flush();
+        };
+
+        try {
+            $this->handleForm($request, $handler);
+            return $this->twig->render('news/admin/success.html.twig');
+        } catch (InvalidFormException $e) {
+            return $this->twig->render(
+                'news/admin/create.html.twig',
+                ['form' => $e->getForm()->createView()]
+            );
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param Article $article
+     * @return string
+     */
+    public function editArticleAction(Request $request, Article $article)
+    {
+        $handler = function() {
+            $this->em->flush();
+        };
+
+        try {
+            $this->handleForm($request, $handler, $article, 'PUT');
+            return $this->twig->render('news/admin/success.html.twig');
+        } catch (InvalidFormException $e) {
+            return $this->twig->render(
+                'news/admin/create.html.twig',
+                ['form' => $e->getForm()->createView()]
+            );
+        }
+    }
+
+    /**
+     * @param  Article $article
+     * @return string
+     */
+    public function deleteArticleAction(Article $article)
+    {
+        $this->em->remove($article);
+        $this->em->flush();
+
+        return $this->twig->render('news/admin/success.html.twig');
+    }
+
+    /**
+     * @param Request  $request
+     * @param \Closure $handler
+     * @param Article  $article
+     * @return \Symfony\Component\Form\Form
+     * @throws \Application\Exception\InvalidFormException
+     */
+    private function handleForm(Request $request, \Closure $handler, Article $article = null)
+    {
+        $form = $this->createArticleForm($article);
+        $form->handleRequest($request);
+
+        if (true === $form->isValid()) {
+            $handler($form);
+            return $form;
+        }
+
+        throw new InvalidFormException($form);
+    }
+
+    /**
+     * @param Article $article
+     * @return \Symfony\Component\Form\Form
+     */
+    private function createArticleForm(Article $article = null)
+    {
+        return $this->form->createBuilder('form', $article, ['data_class' => 'Application\Entity\Article'])
+                           ->add('title', 'text', ['required' => true])
+                           ->add('content', 'textarea', ['required' => true])
+                           ->add('Post', 'submit')
+                           ->getForm();
+
     }
 }
