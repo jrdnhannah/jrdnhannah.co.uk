7560d551e47b3c365759a32f01b539e94f689b32
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-17 14:25:23 +0100

    [Routes] Add CRUD for admin, redirect /admin to create news article, add login action

--- app/config/routes.php
+++ app/config/routes.php
@@ -23,4 +23,31 @@ $app->get('/news/{article}',    'news.controller:showArticleAction')
         }
     );
 
+$app->match('/admin/article',     'news.controller:createArticleAction')
+    ->method('GET|POST')
+    ->bind('route.admin.news_article');
+
+$app->match('/admin/article/{article}/edit', 'news.controller:editArticleAction')
+    ->method('GET|POST|PUT')
+    ->bind('route.admin.edit_news_article')
+    ->convert('article', function ($article) use ($entityParamConverter) {
+            return $entityParamConverter('Application\Entity\Article', $article);
+        }
+    );
+
+$app->match('/admin/article/{article}/delete', 'news.controller:deleteArticleAction')
+    ->method('GET|DELETE')
+    ->bind('route.admin.delete_news_article')
+    ->convert('article', function ($article) use ($entityParamConverter) {
+            return $entityParamConverter('Application\Entity\Article', $article);
+        }
+    );
+
+$app->get('/admin', function() {
+        return new \Symfony\Component\HttpFoundation\RedirectResponse('/admin/article');
+    }
+);
+
+$app->get('/login', 'auth.controller:loginAction');
+
 return $app;
