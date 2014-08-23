b9eaa5e24b311df1ba3cd2ee0d1693c31dc12f3b
jrdn hannah <jrdn@jrdnhannah.co.uk>
2014-08-23 19:39:57 +0100

    [Controller] Add portfolio controller

---
+++ src/Application/Controller/PortfolioController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\Controller;
+
+use Application\Entity\AdminAccessible;
+
+class PortfolioController extends FormHandlerController
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEntityName()
+    {
+        return 'Portfolio Item';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getGroupedName()
+    {
+        return 'Portfolio';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEntityClass()
+    {
+        return 'Application\Entity\PortfolioItem';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function createForm(AdminAccessible $entity = null)
+    {
+        return $this->getFormFactory()
+                    ->createBuilder('form', $entity, ['data_class' => $this->getEntityClass()])
+                    ->add('title', 'text', ['required' => true])
+                    ->add('Add', 'submit')
+                    ->getForm();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getRoutes()
+    {
+        return [
+            'list'   => 'route.portfolio',
+            'single' => 'route.portfolio_item',
+            'confirm_delete' => 'route.admin.confirm_delete_portfolio_item',
+            'delete' => 'route.admin.delete_portfolio_item',
+            'edit'   => 'route.admin.edit_portfolio_item',
+            'create' => 'route.admin.portfolio_item'
+        ];
+    }
+}
